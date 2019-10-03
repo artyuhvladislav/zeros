62ae910ab776016445075ce4a193550053a7b09c
artyuhvladislav <workto22@gmail.com>
2019-10-03 19:33:20 +0300

    fix: add new solution

--- src/index.js
+++ src/index.js
@@ -1,51 +1,40 @@
 module.exports = function zeros(expression) {
 	let expMass = expression.split('*');
-	let expMassSecond = [];
-
-	for(let i = 0; i < expMass.length; i++){
-		if((expMass[i].slice(expMass[i].length-2)) === '!!'){
-			let index;
-			index = expMass.indexOf(expMass[i]);
-			expMassSecond.push(expMass[i]);
-			expMass.splice(index, 1);
-		}
-	};
-
-	let   expMassSecondStr = expMassSecond.join(' ');
-	let   numMassSecondStr = expMassSecondStr.split('!!');
-
-	numMassSecondStr.pop();
-
-	let numMassSecond = [];
-
-	for (let i = 0; i < numMassSecondStr.length; i++){
-		numMassSecond.push(+numMassSecondStr[i]);
-	};
-
-	let counterSecond = 0;
-
-	for (let i = 0; i < numMassSecond.length; i++){
-		for (let j = 10; numMassSecond[i]/j >= 1; j *= 10){
-			counterSecond += Math.floor(numMassSecond[i]/j);
-		}
-	};
-
-	let expMassStr = expMass.join(' ');
-	let   numMassStr = expMassStr.split('!');
-
-	numMassStr.pop();
-
-	let numMass = [];
-	for (let i = 0; i < numMassStr.length; i++){
-		numMass.push(+numMassStr[i]);
-	};
+	let oneFactorial = [];
+	let twoFactorial = [];
 	let counter = 0;
-	for (let i = 0; i < numMass.length; i++){
-		for (let j = 5; numMass[i]/j >= 1; j *= 5){
-			counter += Math.floor(numMass[i]/j);
+	let zerosCount = 0;
+	console.log(expMass);
+	expMass.map(function(item){
+		if(item.endsWith('!!')){
+			twoFactorial.push(+item.slice(0, -2));
 		}
-	}
-	return counter+counterSecond; 
+		else {
+			oneFactorial.push(+item.slice(0, -1));
+		}
+	})
+	twoFactorial.map(function(number) {
+		if (number % 2 === 0) {
+			counter += 1;
+		}
+		for (let i = number; i > 0; i -= 2) {
+			if (i % 5 === 0) {
+				zerosCount += 1;
+				if (i % 25 === 0) {
+					zerosCount += 1;
+				}
+			}
+		}
+	})
+	oneFactorial.map(function(number){
+		zerosCount += Math.floor(number / 5) + Math.floor(number / 25);
+		counter += 1;
+	});
+	if(counter == 0){
+		return 0;
+	} 
+	console.log(counter)
+	return zerosCount;
 }
 
 
